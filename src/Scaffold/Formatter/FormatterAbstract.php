56ce8785e9efad0b24cee11dcb95c63e27646b00
Joe Green <joe.green0991@gmail.com>
2014-03-26 11:09:12 +0000

    Update FormatterAbstract.php

--- src/Scaffold/Formatter/FormatterAbstract.php
+++ src/Scaffold/Formatter/FormatterAbstract.php
@@ -1,11 +1,13 @@
 <?php namespace Scaffold\Formatter;
 
+use Illuminate\Database\Query\Builder as Builder;
+
 abstract class FormatterAbstract implements FormatterInterface
 {
     protected $data;
     protected $elements;
     
-    public function __construct(array $elements, array $data)
+    public function __construct(array $elements, Builder $data)
     {
         $this->data = $data;
         $this->elements = $elements;
