b5fdb1540b8dfe33a27db165a6f7f076896fe356
Joe Green <joe.green@flashtalking.com>
2014-03-31 11:33:36 +0100

    added filters

--- src/Scaffold/Formatter/FormatterAbstract.php
+++ src/Scaffold/Formatter/FormatterAbstract.php
@@ -8,11 +8,37 @@ abstract class FormatterAbstract implements FormatterInterface
     
     protected $elements;
     
+    protected $filterEquals = array();
+    
+    protected $filterNotEquals = array();
+    
     public function __construct(array $elements, ScaffoldDataSet $data = null)
     {
         $this->elements = $elements;
         
         $this->data = $data;
+    }
+    
+    protected function filterElements()
+    {
+        return array_filter($this->elements, function($element){
+            
+            foreach($this->filterNotEquals as $column => $filter)
+            {
+                if(array_get($element, $column) !== $filter)
+                {
+                    return true;
+                }
+            }
+            
+            foreach($this->filterEquals as $column => $filter)
+            {
+                if(array_get($element, $column) === $filter)
+                {
+                    return true;
+                }
+            }
+        });
     }
     
     public function __toString()
